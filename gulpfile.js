9f6790a669a0603c10eddbfe1e9a392f4234d7d3
Tohsaka Rin <saberization@qq.com>
2019-02-19 15:25:52 +0800

    update API

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,17 @@ gulp.task('babelJs', function() {
   return gulp.src('./src/watermark.js')
     .pipe(gulpBabel())
     .pipe(uglify())
+    .on('error', function(error) {
+      console.error(error.toString());
+      this.emit('end');
+    })
     .pipe(rename({
       suffix: '.min'
-    }));
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('default', ['babelJs']);
+gulp.task('watch', function () {
+  gulp.watch('./src/watermark.js', ['babelJs']);
 });
--- src/watermark.js
+++ src/watermark.js
@@ -1,5 +1,11 @@
-module.exports = class WaterMark {
-  constructor() {
-
+(function(global, factory) {
+  typeof exports === 'object' && module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory()) : global.WaterMark = factory(); 
+}(window, function() {
+  class WaterMark {
+    constructor(options) {
+      console.log(options);
+    }
   }
-}
+
+  return WaterMark;
+}));
